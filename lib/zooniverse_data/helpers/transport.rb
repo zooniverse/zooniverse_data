c57417a5e539f325b7b8c3a35477d7f30b647e17
Michael Parrish <michael@zooniverse.org>
2014-02-04 12:42:12 -0600

    Prevent zombie processes from timedout exec calls

--- lib/zooniverse_data/helpers/transport.rb
+++ lib/zooniverse_data/helpers/transport.rb
@@ -15,9 +15,7 @@ module ZooniverseData
       def download(from: nil, to: nil, timeout: 60)
         _with_retries(20) do
           `rm -f '#{ to }'`
-          Timeout::timeout(timeout) do
-            `wget '#{ from }' -t 50 -c -q -O '#{ to }'`
-          end
+          spawn_with_timeout command: "wget '#{ from }' -t 50 -c -q -O '#{ to }'", timeout: timeout
           
           raise 'File not downloaded' unless File.exists?(to)
           raise 'File is empty' unless File.new(to).size > 0
@@ -47,6 +45,20 @@ module ZooniverseData
       
       def bucket_path
         @bucket_path ||= manifest.project.bucket_path
+      end
+      
+      def spawn_with_timeout(command: command, timeout: timeout)
+        pid = Process.spawn command, close_others: true
+        
+        begin
+          Timeout::timeout(timeout) do
+            Process.wait pid
+          end
+        rescue => e
+          Process.kill 'TERM', pid
+          Process.detach pid
+          raise e
+        end
       end
       
       def _with_retries(retries)
