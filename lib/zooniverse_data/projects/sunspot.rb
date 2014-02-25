3d8af50207aafe155232c2762e3f620a8f93e4e3
Michael Parrish <michael@zooniverse.org>
2014-02-24 18:44:59 -0600

    Sunspot: don't set nil locations

--- lib/zooniverse_data/projects/sunspot.rb
+++ lib/zooniverse_data/projects/sunspot.rb
@@ -6,8 +6,12 @@ module ZooniverseData
       def customize_subject
         standard = convert_to_jpeg entry.location['standard']
         inverted = convert_image(standard.path, remove_original: false).invert.write_to(prefix: 'inverted')
-        context_image = convert_to_jpeg(entry.location['context']) if entry.location['context']
-        set_location standard: standard.path, inverted: inverted.path, context: context_image.try(:path)
+        if entry.location['context']
+          context_image = convert_to_jpeg(entry.location['context'])
+          set_location standard: standard.path, inverted: inverted.path, context: context_image.try(:path)
+        else
+          set_location standard: standard.path, inverted: inverted.path
+        end
       end
     end
   end
