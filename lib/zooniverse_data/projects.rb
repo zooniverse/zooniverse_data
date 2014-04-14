5375b6901216899c1f55f8c16bb327dc301d8c68
Michael Parrish <michael@zooniverse.org>
2014-04-14 14:11:14 -0500

    Condor image resizing

--- lib/zooniverse_data/projects.rb
+++ lib/zooniverse_data/projects.rb
@@ -2,6 +2,7 @@ module ZooniverseData
   module Projects
     require 'zooniverse_data/projects/default'
     require 'zooniverse_data/projects/asteroid'
+    require 'zooniverse_data/projects/condor'
     require 'zooniverse_data/projects/milky_way'
     require 'zooniverse_data/projects/serengeti'
     require 'zooniverse_data/projects/sunspot'
--- lib/zooniverse_data/projects/condor.rb
+++ lib/zooniverse_data/projects/condor.rb
@@ -1,7 +1,26 @@
+require 'ostruct'
+
 module ZooniverseData
   module Projects
-    class Condor < Default
+    class Condor
+      include Helpers
       
+      def customize_subject
+        original = convert_image entry.location['standard']
+        dimensions = original.input_image.size rescue OpenStruct.new(width: nil, height: nil)
+        resized = original.resize(width: 1200, force: false)
+          .quality(80)
+          .write_to(prefix: 'resized')
+          .path
+        
+        entry.update :$set => {
+          'location.standard' => resized,
+          'metadata.original_size' => {
+            width: dimensions.width,
+            height: dimensions.height
+          }
+        }
+      end
     end
   end
 end
