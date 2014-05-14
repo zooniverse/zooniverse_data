ef2bb8b9f93faad6e4621322cadad3255e6b3648
Campbell Allen <campbell.allen@gmail.com>
2014-05-14 17:35:57 +0100

    add thumbs to penguin project

--- lib/zooniverse_data/projects/penguin.rb
+++ lib/zooniverse_data/projects/penguin.rb
@@ -4,17 +4,26 @@ module ZooniverseData
       include Helpers
 
       def customize_subject
-        original = convert_image(entry.location['standard'])
-        dimensions = original.input_image.size rescue OpenStruct.new(width: nil, height: nil)
-        resized = original.resize(width: 1_000, height: 1_000, force: false)
-          .quality(80)
-          .write_to(prefix: 'resized')
-          .path
+        original = convert_image(entry.location['standard']).input_image
+        original_dims = original.size rescue OpenStruct.new(width: nil, height: nil)
+        resized = converter_for(original.path, type: 'standard', max_size: 1000)
+        thumb = converter_for(original.path, type: 'thumbnail', max_size: 400, quality: 50)
         entry.update :$set => {
           'location.standard' => resized,
-          'metadata.original_size' => { width: dimensions.width, height: dimensions.height }
+          'location.thumb' => thumb,
+          'metadata.original_size' => { width: original_dims.width, height: original_dims.height }
         }
       end
+
+      private
+
+        def converter_for(path, type: type, max_size: max_size, quality: 80)
+          convert_image(path, remove_original: false)
+            .resize(width: max_size, height: max_size, force: false)
+            .quality(quality)
+            .write_to(prefix: type)
+            .path
+        end
     end
   end
 end
