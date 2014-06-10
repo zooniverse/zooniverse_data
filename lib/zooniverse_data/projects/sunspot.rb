057643f2faccaab0beabd7d2bd5f7c8558bfe9d7
Michael Parrish <michael@zooniverse.org>
2014-06-10 17:08:31 -0500

    Sunspot processing

--- lib/zooniverse_data/projects/sunspot.rb
+++ lib/zooniverse_data/projects/sunspot.rb
@@ -4,8 +4,12 @@ module ZooniverseData
       include Helpers
       
       def customize_subject
-        standard = convert_to_jpeg entry.location['standard']
-        inverted = convert_image(standard.path, remove_original: false).invert.write_to(prefix: 'inverted')
+        standard_path = entry.location['standard']
+        standard_jpg_path = standard_path.sub '.eps', '.jpg'
+        
+        standard = convert_image(standard_path).resize(width: 360, height: 360, force: true).write_to path: standard_jpg_path
+        inverted = convert_image(standard.path, remove_original: false).invert.write_to prefix: 'inverted'
+        
         if entry.location['context']
           context_image = convert_to_jpeg(entry.location['context'])
           set_location standard: standard.path, inverted: inverted.path, context: context_image.try(:path)
