54bdadc978409d74761a185d4a4783abaa795ed4
Adam McMaster <adam@zooniverse.org>
2015-01-16 17:14:41 +0000

    Use paths in filelist.csv rather than recursing S3
    Waaaay simpler, and avoids any possibility of accidentally including
    extra files that were on S3 before the current batch of data was
    uploaded.

--- manifests/generic.rb
+++ manifests/generic.rb
@@ -13,7 +13,7 @@ class GenericManifest
   InvalidMetadata = Class.new(StandardError)
   DupImageName = Class.new(StandardError)
 
-  CSV_METADATA_FILE_REGEX = "filelist([0-9]*)\.(txt|csv)"
+  CSV_FILELIST_NAME = "filelist.csv"
   IMAGE_FILE_REGEX = "\.(jp(e)?g|png)$"
   PROJECT_DATA_PATH = "project_data"
 
@@ -33,10 +33,10 @@ class GenericManifest
 
   def prepare
     load_csv_image_metadata
-    image_paths = construct_image_paths
 
-    image_paths.each do |hash|
-      subject location: url_of(hash[:path]), metadata: hash
+    @csv_image_metadata.each_pair do |file_path, metadata|
+      puts file_path
+      subject location: url_of(file_path), metadata: metadata
     end
   end
 
@@ -46,32 +46,11 @@ class GenericManifest
 
   private
 
-    def strip_prefix_s3_object_key(s3_obj_key)
-      s3_obj_key.match(/#{PROJECT_DATA_PATH}\/#{project_name}\/(.+)/)[1]
-    end
-
-    def construct_image_paths
-      [].tap do |images|
-        subject_count = 0
-        s3_bucket_objects.each do |obj|
-          next unless subject_image?(obj)
-          csv_metadata_key = strip_prefix_s3_object_key(obj.key)
-          images << (@csv_image_metadata[csv_metadata_key] || {}).merge!({ path: csv_metadata_key })
-          subject_count += 1
-          break if @sample && subject_count == @sample_size
-        end
-      end
-    end
-
     def load_csv_image_metadata
-      s3_bucket_objects.each do |obj|
-        next unless obj.key.match(/#{CSV_METADATA_FILE_REGEX}/i)
-        csv_file_data = CSV.parse(obj.read)
-        metadata_csv_file_name = strip_prefix_s3_object_key(obj.key)
-        @header_row = csv_file_data.shift
-
-        read_csv_file_rows(csv_file_data, metadata_csv_file_name)
-      end
+      filelist = zooniverse_data_bucket.objects["#{PROJECT_DATA_PATH}/#{project_name}/#{CSV_FILELIST_NAME}"]
+      csv_file_data = CSV.parse(filelist.read)
+      @header_row = csv_file_data.shift
+      read_csv_file_rows(csv_file_data)
     end
 
     def s3
@@ -82,35 +61,17 @@ class GenericManifest
       @zoo_data_bucket ||= s3.buckets['zooniverse-data']
     end
 
-    def s3_bucket_objects
-      zooniverse_data_bucket.objects.with_prefix("#{PROJECT_DATA_PATH}/#{project_name}/")
-    end
-
-    def read_csv_file_rows(csv_file_data, metadata_csv_file_name)
+    def read_csv_file_rows(csv_file_data)
       csv_file_data.each do |row|
         row.unshift(nil) if row.length != @header_row.length && row.first.match(/#{IMAGE_FILE_REGEX}/i)
         row = row.map! { |val| val && (val.empty? || val.match(/na/i)) ? nil : val }
-        image_file_name_key = construct_image_file_name_key(metadata_csv_file_name, row[0])
-        @csv_image_metadata[image_file_name_key] = {}
+        @csv_image_metadata[row[0]] = {}
         current_col = 0
         row.each do |col|
-          @csv_image_metadata[image_file_name_key][current_col == 0 ? :path : @header_row[current_col]] = col
+          @csv_image_metadata[row[0]][current_col == 0 ? :path : @header_row[current_col]] = col
           current_col += 1
         end
       end
-    end
-
-    def construct_image_file_name_key(metadata_csv_file_name, image_file_name)
-      if metadata_csv_file_name.match(/#{CSV_METADATA_FILE_REGEX}/i)
-        metadata_prefix_name = image_file_name
-      else
-        metadata_prefix_name = metadata_csv_file_name.match(/(.+\/)#{CSV_METADATA_FILE_REGEX}/i)[1]
-        metadata_prefix_name + "/#{image_file_name}"
-      end
-    end
-
-    def subject_image?(s3_obj)
-      s3_obj.key.match(/#{IMAGE_FILE_REGEX}/i)
     end
 end
 
