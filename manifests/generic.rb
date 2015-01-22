c0454526a0e61286baf01884e6f2dc8f1ff52687
Adam McMaster <adam@zooniverse.org>
2015-01-22 17:22:15 +0000

    Add support for coords, groups, and regex override

--- manifests/generic.rb
+++ manifests/generic.rb
@@ -15,7 +15,7 @@ class GenericManifest
 
   CSV_FILELIST_NAME = "filelist.csv"
   IMAGE_FILE_REGEX = "\.(jp(e)?g|png)$"
-  SUBJECT_META_REGEX = "(.+\/)*(?<group>.+)\/atlantis(?<view>.+)_(?<key>[0-9]+_[0-9]+)#{IMAGE_FILE_REGEX}"
+  SUBJECT_META_REGEX = "((?<group_name>[a-zA-Z0-9_-]+)/)(?<key>.+)#{IMAGE_FILE_REGEX}"
   PROJECT_DATA_PATH = "project_data"
 
   def initialize
@@ -26,7 +26,14 @@ class GenericManifest
       })
     end
 
+    if ARGV[1]
+      @subject_meta_regex = ARGV[1]
+    else
+      @subject_meta_regex = SUBJECT_META_REGEX
+    end
+
     @csv_image_metadata = Hash.new { |h,k| h[k] = { :location => [], :metadata => {} } }
+    @group_metadata = Hash.new { |h,k| h[k] = { :metadata => {} } }
     @sample = false
     @sample_size = 200
     @header_row = []
@@ -35,8 +42,13 @@ class GenericManifest
   def prepare
     load_csv_image_metadata
 
+    @group_metadata.each_pair do |group_key, group_hash|
+      group_hash[:name] = group_key
+      group group_hash
+    end
+
     @csv_image_metadata.each_pair do |subject_key, subject_hash|
-      subject location: subject_hash[:location], metadata: subject_hash[:metadata]
+      subject subject_hash
     end
   end
 
@@ -66,18 +78,29 @@ class GenericManifest
         row.unshift(nil) if row.length != @header_row.length && row.first.match(/#{IMAGE_FILE_REGEX}/i)
         row = row.map! { |val| val && (val.empty? || val.match(/na/i)) ? nil : val }
 
-        subject_match = row[0].match(/#{SUBJECT_META_REGEX}/)
-
+        subject_match = row[0].match(/#{@subject_meta_regex}/)
         @csv_image_metadata[subject_match[:key]][:location].push(url_of(row[0]))
-        @csv_image_metadata[subject_match[:key]][:metadata][:image_metadata] = @csv_image_metadata[subject_match[:key]][:metadata].fetch(:image_metadata, [])
-        @csv_image_metadata[subject_match[:key]][:metadata][:image_metadata].push({ :view => subject_match[:view], :name => row[0], :group => subject_match[:group] })
+
+        if subject_match[:group_name]
+          @group_metadata[subject_match[:group_name]] = { :metadata => {} }
+          @csv_image_metadata[subject_match[:key]][:group_name] = subject_match[:group_name]
+        end
 
         # Skip col 0
         current_col = 1
         row.shift
+        coords = {}
         row.each do |col|
-          @csv_image_metadata[subject_match[:key]][:metadata][@header_row[current_col]] = col
+          if @header_row[current_col] == 'latitude' or @header_row[current_col] == 'longitude'
+            coords[@header_row[current_col]] = col
+          else
+            @csv_image_metadata[subject_match[:key]][:metadata][@header_row[current_col]] = col
+          end
           current_col += 1
+        end
+
+        if coords['latitude'] and coords['longitude']
+           @csv_image_metadata[subject_match[:key]][:coords] = [ coords['longitude'], coords['latitude'] ]
         end
       end
     end
