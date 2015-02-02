caa550da1007d760593bb1386f42a79fd47c0e1e
Michael Parrish <michael@zooniverse.org>
2015-02-02 14:55:08 -0600

    Serengeti season 8

--- manifests/serengeti.rb
+++ manifests/serengeti.rb
@@ -2,23 +2,25 @@ require_relative 'manifest'
 
 class Serengeti
   include Manifest
-  attr_accessor :data
+  attr_accessor :data, :sites
   
   def initialize
     self.data = { }
+    self.sites = load_json(file_named('sites.json'))
   end
   
   def prepare
-    group name: 'season_7'
+    group name: 'season_8'
     load_data
     create_subjects
   end
   
   def load_data
-    load_json(file_named('season7.json')).each do |hash|
-      next if hash['include'] == 'exclude'
-      hash['site_roll_code'] = "S7_#{ hash['site'] }_R#{ hash['roll'] }"
+    load_json(file_named('season8.json')).each do |hash|
+      next if hash['invalid'] == '1'
+      hash['site_roll_code'] = "S8_#{ hash['site'] }_R#{ hash['roll'] }"
       key = "#{ hash['site_roll_code'] }_#{ hash['capture'] }"
+      hash['newtime'] = timestamp_of hash['newtime']
       self.data[key] ||= { }
       self.data[key][hash['image']] = hash
     end
@@ -27,6 +29,7 @@ class Serengeti
   def create_subjects
     data.each do |key, capture|
       images = capture.values_at('1', '2', '3').compact
+      coords = sites[images.first['site']] || []
       locations = []
       filenames = []
       timestamps = []
@@ -37,13 +40,19 @@ class Serengeti
         timestamps << image['newtime']
       end
       
-      subject group_name: 'season_7', location: locations, metadata: {
+      subject group_name: 'season_8', location: locations, coords: coords, metadata: {
         capture_event_id: images.first['capture'].to_i,
         filenames: filenames,
         site_roll_code: images.first['site_roll_code'],
         timestamps: timestamps
       }
     end
+  end
+  
+  def timestamp_of(timestamp)
+    date, time = timestamp.split ' '
+    month, day, year = date.split '/'
+    Time.parse("#{ year }-#{ month }-#{ day } #{ time } +0300").utc
   end
 end
 
