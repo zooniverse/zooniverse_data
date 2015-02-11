8fce18cbd422cff15f8f22c972fd81dfc3849b49
Adam McMaster <adam@zooniverse.org>
2015-02-11 11:55:52 +0000

    Add Illustrated Life

--- lib/zooniverse_data/projects.rb
+++ lib/zooniverse_data/projects.rb
@@ -4,6 +4,7 @@ module ZooniverseData
     require 'zooniverse_data/projects/asteroid'
     require 'zooniverse_data/projects/condor'
     require 'zooniverse_data/projects/higgs_hunter'
+    require 'zooniverse_data/projects/illustratedlife'
     require 'zooniverse_data/projects/milky_way'
     require 'zooniverse_data/projects/penguin'
     require 'zooniverse_data/projects/serengeti'
---
+++ lib/zooniverse_data/projects/illustratedlife.rb
@@ -0,0 +1,27 @@
+module ZooniverseData
+  module Projects
+    class IllustratedLife
+      include Helpers
+
+      def customize_subject
+        original = convert_image(entry.location['standard']).input_image
+        original_dims = original.size rescue OpenStruct.new(width: nil, height: nil)
+        resized = converter_for(original.path, type: 'standard', max_size: 1400)
+        entry.update :$set => {
+          'location.standard' => resized,
+          'metadata.original_size' => { width: original_dims.width, height: original_dims.height }
+        }
+      end
+
+      private
+
+        def converter_for(path, type: type, max_size: max_size, quality: 80)
+          convert_image(path, remove_original: false)
+            .resize(width: max_size, height: max_size, force: false)
+            .quality(quality)
+            .write_to(prefix: type)
+            .path
+        end
+    end
+  end
+end
