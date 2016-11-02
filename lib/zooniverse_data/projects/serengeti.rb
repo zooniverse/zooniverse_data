5835c7b49837ff1823dbe422fa852a2176b993d0
Adam McMaster <adam@zooniverse.org>
2016-11-02 14:25:29 +0000

    Revert "Don't crop serengeti images"
    This reverts commit 961c3071128af6b808833d70d1ac4978844a6cb8.
    
    Closes #7

--- lib/zooniverse_data/projects/serengeti.rb
+++ lib/zooniverse_data/projects/serengeti.rb
@@ -2,30 +2,31 @@ module ZooniverseData
   module Projects
     class Serengeti
       include Helpers
-
+      
       def customize_subject
         new_locations = {
           'standard' => [],
           'thumbnail' => [],
           'large' => []
         }
-
+        
         entry.location['standard'].each do |path|
           large_image = large_converter path
           new_locations['large'] << large_image
           new_locations['standard'] << converter_for(large_image, type: 'standard', max_size: 600)
           new_locations['thumbnail'] << converter_for(large_image, type: 'thumbnail', max_size: 300)
         end
-
+        
         set_location new_locations
       end
-
+      
       def large_converter(path)
         convert_image(path)
+          .command('-gravity South -chop 0x100')
           .write_to(prefix: 'large')
           .path
       end
-
+      
       def converter_for(path, type: nil, max_size: nil)
         convert_image(path, remove_original: false)
           .resize(width: max_size, height: max_size, force: false)
